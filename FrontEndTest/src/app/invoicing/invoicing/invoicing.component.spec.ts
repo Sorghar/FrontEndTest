6f05050d748a34baec02e66bdb0e9f3b2128a274
Martin Mikula <martin.mikula@kros.sk>
2020-07-24 16:25:19 +0200

    Test imports

--- FrontEndTest/src/app/invoicing/invoicing/invoicing.component.spec.ts
+++ FrontEndTest/src/app/invoicing/invoicing/invoicing.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { InvoicingComponent } from './invoicing.component';
 
@@ -8,7 +9,13 @@ describe('InvoicingComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ InvoicingComponent ]
+      declarations: [
+        InvoicingComponent
+      ],
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+      ]
     })
     .compileComponents();
   }));
